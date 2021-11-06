b531dc67f719ae495cd595a8f66e7c510432d13a
phamtanphat <phatdroid94@gmail.com>
2021-11-06 21:20:01 +0700

    xử lý foreground

--- app/src/main/java/com/example/boundservice26082021/MyService.java
+++ app/src/main/java/com/example/boundservice26082021/MyService.java
@@ -1,19 +1,29 @@
 package com.example.boundservice26082021;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
 
     OnListenerCountChange onListenerCountChange;
-
+    Notification notification;
+    NotificationManager notificationManager;
     class MyBinder extends Binder {
         MyService getService() {
             return MyService.this;
@@ -35,6 +45,9 @@ public class MyService extends Service {
     public void onCreate() {
         Log.d("BBB","onCreate");
         super.onCreate();
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notification = createNotification(0);
+        startForeground(1,notification);
     }
 
     @Override
@@ -54,11 +67,21 @@ public class MyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("BBB","onStartCommand");
         if (!isRunning) {
+            Handler handler = new Handler(new Handler.Callback() {
+                @Override
+                public boolean handleMessage(@NonNull Message msg) {
+                    if (msg.what == 1){
+                        notification = createNotification((Integer) msg.obj);
+                        notificationManager.notify(1,notification);
+                    }
+                    return false;
+                }
+            });
             thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     isRunning = true;
-                    for (int i = 0; i < 100; i++) {
+                    for (int i = 1; i < 100; i++) {
                         if(onListenerCountChange != null){
                             onListenerCountChange.onChanged(i);
                         }
@@ -67,6 +90,10 @@ public class MyService extends Service {
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
+                        Message message = new Message();
+                        message.what = 1;
+                        message.obj = i;
+                        handler.dispatchMessage(message);
                     }
 
                 }
@@ -88,5 +115,29 @@ public class MyService extends Service {
 
     public void setOnListenerCountChange(OnListenerCountChange onListenerCountChange){
         this.onListenerCountChange = onListenerCountChange;
+    }
+
+    public Notification createNotification(int count){
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID");
+        builder.setSmallIcon(android.R.drawable.ic_dialog_email);
+        builder.setShowWhen(true);
+        builder.setContentTitle("Xử lý data");
+        builder.setContentText("Count " + count);
+        builder.setContentIntent(pendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("CHANNEL_ID", "CHANNEL", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        return builder.build();
     }
 }

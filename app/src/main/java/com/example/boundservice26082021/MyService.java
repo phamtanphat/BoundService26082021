25691cb012e6d583d5115dbed7254c8879352bb2
phamtanphat <phatdroid94@gmail.com>
2021-11-06 20:14:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/boundservice26082021/MyService.java
@@ -0,0 +1,72 @@
+package com.example.boundservice26082021;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+
+    class MyBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    boolean isRunning;
+    Thread thread;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d("BBB","onBind");
+        return new MyBinder();
+    }
+
+
+    @Override
+    public void onCreate() {
+        Log.d("BBB","onCreate");
+        super.onCreate();
+    }
+
+    @Override
+    public void unbindService(ServiceConnection conn) {
+        super.unbindService(conn);
+        Log.d("BBB","unBindservice");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("BBB","onStartCommand");
+        if (!isRunning) {
+            thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    isRunning = true;
+                    for (int i = 0; i < 50; i++) {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("BBB","onDestroy");
+        super.onDestroy();
+    }
+}
